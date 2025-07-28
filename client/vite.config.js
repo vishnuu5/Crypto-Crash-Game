7fba18833d8ee22d00fc150803b0dafd6d73b596
vishnu <139614960+vishnuu5@users.noreply.github.com>
2025-07-28 18:21:29 +0530

    vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
 });
